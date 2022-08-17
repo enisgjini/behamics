d8a376b069db07cb5d5f381bb199dc650d7bdd54
Enis Gjini <gjinienis148@gmail.com>
2022-08-17 19:37:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Task - 5
+
+var product = {
+  name: "VANS Ward Kids Sneaker",
+  price: "59.90 CHF",
+  imageURL:
+    "https://ochsnersport.scene7.com/asset/ochsnersport/product/detail/image/preset/v3-pdp-image/w1000/fit/fit-1/vans-ward-kinder-sneaker-1--01_1090001_P.jpg",
+  url: "https://www.ochsnersport.ch/de/shop/vans-ward-kinder-sneaker-schwarz-00002001674736-p.html",
+  color: "black",
+};
+
+console.log(product);
+
+// Task - 6
+
+const products = [
+  {
+    name: "Puma",
+    price: "49.90",
+  },
+  {
+    name: "ASICS",
+    price: "49.90",
+  },
+  {
+    name: "BEACH MOUNTAIN",
+    price: "39.90",
+  },
+];
+
+console.log(products);
